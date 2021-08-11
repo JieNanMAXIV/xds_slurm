4b470d0163235bbed8c8f076ec66b87119aafa10
Jie Nan <jie.nan@maxiv.lu.se>
2021-08-11 13:56:02 +0200

    refactor - simplify fork jobs

--- xds.sh
+++ xds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #an example of sbatch file to submit xds_par job
-#Jie Nan at MAXIV, 08/2019
+#Jie Nan at MAXIV, 08/2021
 #jie.nan@maxiv.lu.se
 
 #SBATCH -t 00:60:00
@@ -26,4 +26,4 @@ export PATH="$MX_SOFT/XDS:$PATH"
 
 # run the program
 # customise for your program name and add arguments if required
-xds_par_ori
+xds_par
